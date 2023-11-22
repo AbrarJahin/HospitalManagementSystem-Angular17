2b6d684df6de27d17000419686652e86b8de779b
Abrar Jahin <abrarjahin@live.com>
2023-11-22 12:02:14 -0500

    Button added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,13 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'HospitalManagementSystem';
+  CallSomeLogic()
+  {
+    alert("Clicked");
+    console.log("clicked");
+  }
 }
